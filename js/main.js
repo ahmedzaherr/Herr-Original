a07e299455e7dc7092ab46807f1d859993486623
ahmedzaherr <ahmed.zaher.tech@gmail.com>
2026-01-13 04:30:32 +0200

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// Initialize Animations
-AOS.init({
-    duration: 1000,
-    once: true
-});
-
-// Smooth scroll logic can be added here if needed
-console.log("Herr Original Website Loaded Successfully");
